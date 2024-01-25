963c9de4c5b3ff71a6d6ffd2fc4b78c86de4194e
vali <chvaly@gmail.com>
2024-01-25 22:53:32 +0200

    INT - Update controller and add h2 config

--- src/main/java/com/vali/library/controller/AuthorController.java
+++ src/main/java/com/vali/library/controller/AuthorController.java
@@ -3,10 +3,10 @@ package com.vali.library.controller;
 import com.vali.library.entity.Author;
 import com.vali.library.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,12 +19,20 @@ public class AuthorController {
 
     @GetMapping
     public List getAllAuthors() {
-        authorService.getAllAuthors();
+        return authorService.getAllAuthors();
     }
 
-    @GetMapping()
-    public Author getAuthor() {
-        authorService.getAuthor()
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable Long id) {
+        return authorService.getAuthor(id)
+                .map(author -> new ResponseEntity<>(author, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+        Author savedAuthor = authorService.createAuthor(author);
+        return new ResponseEntity<>(savedAuthor, HttpStatus.CREATED);
     }
 
 
